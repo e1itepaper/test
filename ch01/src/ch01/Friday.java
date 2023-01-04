fd9434080280228a21d68e69bfd6c49f7b85ea72
YEJI <YEJI@DESKTOP-ULBH3K1>
2023-01-04 17:34:24 +0900

    ch01

---
+++ ch01/src/ch01/Friday.java
@@ -0,0 +1,14 @@
+package ch01;
+/* 작성자 : 정예지
+작성일 : 2022.12.23*/
+public class Friday {
+
+	public static void main(String[] args) {
+		System.out.println("오늘은 금요일");
+		System.out.println("내일부터 이틀 공부하기 좋은 요일이다.");
+//		System.out.println("ㅋㅋ");
+// 		\n : 줄바꿈
+
+	}
+
+}
---
+++ ch01/src/ch01/HelloJava.java
@@ -0,0 +1,16 @@
+package ch01;
+
+public class HelloJava {
+	public static void main(String[] args) {
+		System.out.println("안녕하세요 푸항항");
+//		int result1 = Java(10, 20);
+//		System.out.println(result1);
+
+	}
+
+//	static int Java(int a, int b) {
+//		int result = a + b;
+//		return result;
+//	}
+
+}
